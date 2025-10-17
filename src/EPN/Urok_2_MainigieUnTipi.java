81c0af184109fa6cd184465d8163745b5917f217
emilcks. <emils.glotovs@sk.lvt.lv>
2025-10-17 21:17:17 +0300

    Изменение

---
+++ src/EPN/Urok_2_MainigieUnTipi.java
@@ -0,0 +1,15 @@
+package EPN;
+
+public class Urok_2_MainigieUnTipi {
+public static void main(String[] args) {
+	System.out.println("Переменные и типы данных");
+	
+	int Возраст; // int только целые числа - можно дать значение
+	Возраст = 45;
+	
+	
+	Возраст = 2;
+	
+	System.out.println("int = "+ Возраст);
+}
+}
